84d7ceafd87eb31dcc5e155b2c3eee3265e634d3
edgarguitarist <edgarguitarist@gmail.com>
2021-12-26 20:15:08 -0500

    change php to only html, js and css

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,52 @@ function getMyInfoGit() {
       var user = JSON.parse(xhr.responseText);
       document.getElementById("logo").src = user.avatar_url;
       //cambiar el icon
-        document.getElementById("icon").href = user.avatar_url;
+      document.getElementById("icon").href = user.avatar_url;
     }
   };
   xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+
+//cambiar atributos de un boton cuando se haga click
+function changeButton(elemento) {
+  var button = document.getElementById(elemento.id);
+  if (button.classList.contains("English")) {
+    button.classList.remove("English");
+    button.classList.add("Spanish");
+    button.innerHTML = "<img id='flag_img' src='images/flags/es.png' alt='flag' class='mp-0 h-30'>Es";
+    //flag.src = "images/flags/es.png";
+  } else {
+    button.classList.remove("Spanish");
+    button.classList.add("English");
+    button.innerHTML = "<img id='flag_img' src='images/flags/en.png' alt='flag' class='mp-0 h-30'>En";
+    //flag.src = "images/flags/en.png";
+
+  }
 }
 
 $(document).ready(function () {
