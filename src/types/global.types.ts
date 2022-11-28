e48cd139b7cf5c3792d08b0f9a1bdd4311eb0bd2
edgarguitarist <edgarguitarist@gmail.com>
2022-11-28 00:58:37 -0500

    Adding more Types ⚙️

--- src/types/global.types.ts
+++ src/types/global.types.ts
@@ -1,20 +1,50 @@
 export enum Nav {
-    aboutme = "aboutme",
-    skills = "skills",
-    projects = "projects",
-    knowledge = "knowledge",
-  }
-
+  aboutme = "aboutme",
+  skills = "skills",
+  projects = "projects",
+  knowledge = "knowledge",
+}
+export enum Info {
+  facebook = "facebook",
+  instagram = "instagram",
+  twitter = "twitter",
+  github = "github",
+  pdf = "pdf",
+  pdf2 = "pdf2",
+  linkedin = "linkedin",
+  whatsapp = "whatsapp",
+}
 export type GeneralLinkAttribute = {
-    href: string;
-    //icon: React.ReactElement;
-    style?: string;
-    label?: string | null;
-  };
+  href: string;
+  iconName?: string;
+  style?: string;
+  label?: string | null;
+};
 
 export interface NavDicInterface {
-    [Nav.aboutme]: GeneralLinkAttribute;
-    [Nav.skills]: GeneralLinkAttribute;
-    [Nav.projects]: GeneralLinkAttribute;
-    [Nav.knowledge]: GeneralLinkAttribute;
-  }
+  [Nav.aboutme]: GeneralLinkAttribute;
+  [Nav.skills]: GeneralLinkAttribute;
+  [Nav.projects]: GeneralLinkAttribute;
+  [Nav.knowledge]: GeneralLinkAttribute;
+}
+export interface SocialsInterface {
+  social: Info;
+  className?: string;
+}
+
+export interface SocialDicInterface {
+  [Info.facebook]: GeneralLinkAttribute;
+  [Info.instagram]: GeneralLinkAttribute;
+  [Info.twitter]: GeneralLinkAttribute;
+  [Info.github]: GeneralLinkAttribute;
+  [Info.pdf]: GeneralLinkAttribute;
+  [Info.pdf2]: GeneralLinkAttribute;
+  [Info.linkedin]: GeneralLinkAttribute;
+  [Info.whatsapp]: GeneralLinkAttribute;
+}
+
+export type svgProps = {
+  className?: string;
+  class?: string;
+  props?: {};
+};
