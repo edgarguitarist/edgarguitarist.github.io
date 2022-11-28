23c9fb62eb14d2aaf0d1d10e6572c6a05afb3455
edgarguitarist <edgarguitarist@gmail.com>
2022-11-27 22:52:53 -0500

    Adding Tailwind 🌬️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [astroI18next()],
-    site: 'https://edgarguitarist.github.io',    
-  });
+  integrations: [astroI18next(), tailwind()],
+  site: 'https://edgarguitarist.github.io'
+});
