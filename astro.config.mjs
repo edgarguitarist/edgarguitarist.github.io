e4716cff5b07418bf70bdec62a606517265a514a
edgarguitarist <edgarguitarist@gmail.com>
2022-11-28 00:53:11 -0500

    Adding Preact ⚛️

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), tailwind()],
+  integrations: [astroI18next(), tailwind(), preact()],
   site: 'https://edgarguitarist.github.io'
 });
