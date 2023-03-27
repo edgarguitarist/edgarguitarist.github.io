9f519f8aaf1b33f088f3d28d9c2db77ff0e5f7c7
edgarguitarist <edgarguitarist@gmail.com>
2023-03-26 20:15:41 -0500

    fix: fetching all data

--- scraping/repos.js
+++ scraping/repos.js
@@ -1,8 +1,37 @@
 import { writeDBFile } from "../db/index.js";
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
-const REPOS_URL = "https://api.github.com/users/edgarguitarist/repos";
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const REPOS_URL = process.env.REPOS_URL;
+
+const headers = {
+  Accept: "application/vnd.github.v3+json",
+  Authorization: GITHUB_TOKEN,
+};
+
+let repos = []
 
 export default async () => {
-  writeDBFile("repos", await fetch(REPOS_URL).then((res) => res.json()));
+
+  repos = await fetch(REPOS_URL, { headers }).then((res) => res.json()).catch((err) => console.log(err));
+
+  if (!repos.length) {
+    console.log("No hay repos", repos)
+    return
+  };
+
+  const responses = await Promise.all(
+    repos.map(async (repo) => {
+      let { contributors_url, languages_url, fork } = repo;
+      if (fork) {
+        repo.contributors = await fetch(contributors_url, { headers }).then((res) => res.json())
+      }
+      repo.languages = await fetch(languages_url, { headers }).then((res) => res.json())
+      return repo;
+    })
+  )
+
+  writeDBFile("repos", responses);
   console.log("We got it, my repos data!!!");
 };
