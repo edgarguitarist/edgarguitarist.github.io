088497d875b52147c2174fddde5e1a5fd3e8ea2d
edgarguitarist <edgarguitarist@gmail.com>
2023-04-17 00:29:40 -0500

    fix: use the user

--- scraping/repos.js
+++ scraping/repos.js
@@ -1,9 +1,10 @@
 import { writeDBFile } from "../db/index.js";
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 dotenv.config()
 
+const USER = process.env.USER ?? "edgarguitarist";
 const TOKEN = process.env.TOKEN;
-const REPOS_URL = process.env.REPOS_URL ?? "https://api.github.com/users/edgarguitarist/repos";
+const REPOS_URL = process.env.REPOS_URL ?? `https://api.github.com/users/${USER}/repos`;
 
 const headers = {
   Accept: "application/vnd.github.v3+json",
@@ -25,7 +26,10 @@ export default async () => {
     repos.map(async (repo) => {
       let { contributors_url, languages_url, fork } = repo;
       if (fork) {
-        repo.contributors = await fetch(contributors_url, { headers }).then((res) => res.json())
+        repo.contributor = await fetch(contributors_url, { headers })
+          .then((res) => res.json())
+          .then((res) => res.filter((contributor) => contributor.login === USER))
+          .then((res) => res[0] ?? null);
       }
       repo.languages = await fetch(languages_url, { headers }).then((res) => res.json())
       return repo;
